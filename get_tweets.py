f5e588c2ef571411833057ed30479ff2e510a8e2
Marius Sørensen <jobb@s0rensen.no>
2021-03-23 14:30:33 +0100

    Send more than one request

--- get_tweets.py
+++ get_tweets.py
@@ -4,7 +4,7 @@ import requests
 import os
 from dotenv import load_dotenv
 import json
-
+from tqdm import tqdm
 load_dotenv()
 
 # %%
@@ -13,18 +13,17 @@ tweets = pd.read_table('data/corona_tweets_01.csv',
                         index_col=0,
                         sep=',')
 # %%
-tweet_ids = ','.join([str(item) for item in tweets.index[:10]])
-
-# %%
-headers = {'Authorization': f'Bearer {os.getenv("TWITTER_TOKEN")}'}
-r = requests.get(f'https://api.twitter.com/2/tweets?ids={tweet_ids}', headers = headers)
-response = json.loads(r.content)
-d = response['data']
-# %%
 tweets['Text'] = ''
+headers = {'Authorization': f'Bearer {os.getenv("TWITTER_TOKEN")}'}
+for i in tqdm(range(100)):
+    tweet_ids = ','.join([str(item) for item in tweets.index[100 * i:100 * (i + 1)]])
+    r = requests.get(f'https://api.twitter.com/2/tweets?ids={tweet_ids}', headers = headers)
+    try:
+        response = json.loads(r.content)
+        if 'data' in response:
+            d = response['data']
+            for tweet in d:
+                tweets.loc[int(tweet['id']), 'Text'] = tweet['text']
 
 # %%
-for tweet in d:
-    tweets.loc[int(tweet['id']), 'Text'] = tweet['text']
-
 tweets
