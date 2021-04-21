db23c9fea20d9e6bfab072f0d6a0047cdc788d8e
Marius Sørensen <jobb@s0rensen.no>
2021-04-21 17:08:00 +0200

    Numpy :cry:

--- classify.py
+++ classify.py
@@ -1,17 +1,16 @@
-#! /usr/bin/python
-
 from mrjob.job import MRJob
 import pickle
-import numpy as np
+# import numpy as np
 class MRClassify(MRJob):
     def mapper(self,_, line): 
         id, sentiment, text = line.split(",",2)
         features = pickle.load(open('./reg_features.sav', 'rb'))
 
         sparce_test = [feature not in text for feature in features]
-        sparce_test =np.array(sparce_test).astype(int).reshape(1,-1)
+        # sparce_test = np.array(sparce_test).astype(int).reshape(1,-1)
         lr = pickle.load(open('./reg_params.sav', 'rb'))
-        gen_pred = lr.predict(sparce_test)
+        # gen_pred = lr.predict(sparce_test)
+        gen_pred = 'IKKE BINÆRT >:) '
 
         yield text, [gen_pred,sentiment]
 
