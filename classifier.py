d1bfd32083cb9c4769f4a8a1048e06dc523fd6a5
johannnakinstad <johanna@hebb.no>
2021-04-21 11:23:32 +0200

    does not remove emojis

--- classifier.py
+++ classifier.py
@@ -6,6 +6,7 @@ import nltk
 
 from yellowbrick.text import FreqDistVisualizer
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
@@ -16,22 +17,28 @@ warnings.filterwarnings("ignore")
 
 #%%
 trainData = pd.read_csv(r'data/gender-classifier-DFE-791531.csv',
-                   encoding = "latin1")
+                   encoding = "utf-8")
 
 testData = pd.read_csv(r'data/blog-gender-dataset-clean.csv',names=["gender","description"],
-                   encoding = "latin1")
+                   encoding = "utf-8")
 #%%
-data = trainData.loc[:,["gender", "description"]]
+data = trainData.loc[:,["gender", "description", "tweet_created"]]
 data.dropna(axis = 0, inplace = True)
 data.gender = [1 if gender == "female" else 0 for gender in data.gender]
 
 testData.dropna(axis=0, inplace=True)
-
+data
 # %% natural language processing 
 def nl_processing(data): 
+    emoji_pat = '[\U0001F300-\U0001F64F\U0001F680-\U0001F6FF\u2600-\u26FF\u2700-\u27BF]'
+    shrink_whitespace_reg = re.compile(r'\s{2,}')
     description_list = [] # we created a list so we after these steps, we will append into this list
     for description in data.description:
-        description = re.sub("[^a-zA-Z]", " ", description) # remove non letter signs 
+        reg = re.compile(r'({})|[^a-zA-Z]'.format(emoji_pat)) # line a
+        result = reg.sub(lambda x: ' {} '.format(x.group(1)) if x.group(1) else ' ', description)
+        description =shrink_whitespace_reg.sub(' ', result)
+
+        #description = re.sub("[^a-zA-Z]", " ", description) # remove non letter signs 
         description = description.lower() 
         description = nltk.word_tokenize(description) # list of words
         lemma = nltk.WordNetLemmatizer() 
@@ -43,10 +50,12 @@ def nl_processing(data):
 #%%
 description_list = nl_processing(data)
 description_list_test = nl_processing(testData)
+
+description_list
 # %%
 # Bag of Words
 def bag_words(description_list):
-    max_features = 1500
+    max_features = 5000
     count_vectorizer = CountVectorizer(max_features=max_features,stop_words = "english")
     sparce_matrix = count_vectorizer.fit_transform(description_list).toarray()
     features = count_vectorizer.get_feature_names()
@@ -62,21 +71,19 @@ visualizer.show()
 # %%
 y = data.iloc[:, 0].values
 x = sparce_matrix
-y_test = testData.iloc[:, 0].values
-x_test,_ = bag_words(description_list_test)
+#y_test = testData.iloc[:, 0].values
+#x_test,_ = bag_words(description_list_test)
 
-x_test.shape
+#x_test.shape
 
 # %% splits into train and test subset 
-#x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1, random_state = 42)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1, random_state = 42)
 #%%
 
 # %% Logistic regression
 lr = LogisticRegression(random_state = 42)
-lr.fit(x, y)
-
+lr.fit(x_train, y_train)
 y_pred = lr.predict(x_test)
-print(len(y_pred))
 accuracy = 100.0 * accuracy_score(y_test, y_pred)
 print("Accuracy:{:.3%}".format(accuracy_score(y_test, y_pred)))
 # %%
@@ -89,3 +96,14 @@ y_pred = rf.predict(x_test)
 # Random Forest 
 accuracy = 100.0 * accuracy_score(y_test, y_pred)
 print("Accuracy: ", accuracy)
+
+#%%
+from sklearn.neighbors import KNeighborsClassifier
+rf = KNeighborsClassifier()
+rf.fit(x, y)
+# prediction
+y_pred = rf.predict(x_test)
+# Random Forest 
+accuracy = 100.0 * accuracy_score(y_test, y_pred)
+print("Accuracy: ", accuracy)
+
