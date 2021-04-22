a33fad6274c24da25dffe5a03c5c980bed22eb95
Ove <ove.kristian.jorgensen@gmail.com>
2021-04-22 13:53:02 +0200

    funka ikke med concat, bruker to_csv med append i stedet

--- csv_merger.py
+++ csv_merger.py
@@ -8,9 +8,9 @@ os.chdir('resulting_tweets')
 if len(sys.argv) - 1 == 0 or sys.argv[1] == 'new':
     combined_csv = [pd.read_csv(f, index_col=None, sep=',', header=0) for f in ['thread_1.csv', 'thread_2.csv', 'thread_3.csv']]
 elif sys.argv[1] == 'old':
-    combined_csv = pd.concat([pd.read_csv(f, sep=',') for f in ['old_thread_1.csv', 'old_thread_2.csv', 'old_thread_3.csv']])
+    combined_csv = [pd.read_csv(f, sep=',') for f in ['old_thread_1.csv', 'old_thread_2.csv', 'old_thread_3.csv']]
 elif sys.argv[1] == 'all':
-    combined_csv = pd.concat([pd.read_csv(f, sep=',') for f in glob.glob('*.csv')])
+    combined_csv = [pd.read_csv(f, sep=',') for f in glob.glob('*.csv')]
 
 os.chdir('..')
 
