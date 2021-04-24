ad6043a5e7fc9826590c609feba61a7868293fa4
johannnakinstad <johanna@hebb.no>
2021-04-24 17:01:21 +0200

    preprocess

--- preprocess.py
+++ preprocess.py
@@ -6,7 +6,15 @@ import numpy as np
 import time
 class MRPreprocess(MRJob):
     def mapper(self, _, line): 
-        id, sentiment, text = line.split(",",2)
+        split=line.split(",",2)
+
+        if len(split) != 3:
+
+            id = 12345678
+            sentiment = 0.5
+            text = "okei"
+        else:
+            id,sentiment,text=split 
 
         emoji_pat = '[\U0001F300-\U0001F64F\U0001F680-\U0001F6FF\u2600-\u26FF\u2700-\u27BF]'
         shrink_whitespace_reg = re.compile(r'\s{2,}')
@@ -21,11 +29,11 @@ class MRPreprocess(MRJob):
         description = [lemma.lemmatize(word) for word in description] # find orginal form of word ex. drove -> drive 
         description = " ".join(description) 
 
-        shifted = id >> 22 
+        shifted = int(id) >> 22 
         timestamp = shifted + 1288834974657
         time_created = time.ctime(timestamp/1000)
 
-        string = f'{sentiment},{description},{time_created}'
+        string = f'{sentiment},{time_created},{description}'
 
         yield id, string
     
