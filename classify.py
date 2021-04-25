34f9b09e75378f53d50fea04fe74e941f2aa9491
johannnakinstad <johanna@hebb.no>
2021-04-25 11:43:09 +0200

    small changes

--- classify.py
+++ classify.py
@@ -9,7 +9,7 @@ class MRClassify(MRJob):
         id, sentiment, text, time = line.split(",",2)
         features = pickle.load(open('./reg_features.sav', 'rb'))
 
-        sparce_test = [feature not in text for feature in features]
+        sparce_test = [feature in text for feature in features]
         sparce_test =np.array(sparce_test).astype(int).reshape(1,-1)
         lr = pickle.load(open('./dc_params.sav', 'rb'))
         gen_pred = lr.predict(sparce_test)
