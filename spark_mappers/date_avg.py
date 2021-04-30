bbd0a6199023cca86bf71da746af36f7b2c933f2
Ove <ove.kristian.jorgensen@gmail.com>
2021-04-30 09:58:32 +0200

    file structure improvements and sparkified date_avg

---
+++ spark_mappers/date_avg.py
@@ -0,0 +1,25 @@
+from pyspark.sql import SparkSession
+import sys
+
+def mapper(line):
+    _, string = line.split('\t')
+    string = string.strip("\"")
+    sentiment, date, _, _ = string.split(',',3)
+    return date, float(sentiment)
+
+if __name__ == '__main__': 
+    if len(sys.argv[0] != 3):
+        print('Usage: date_avg.py <input path> <ouput path>', file=sys.stderr)
+        sys.exit(-1)
+    
+    spark = SparkSession\
+        .builder\
+        .appName('date_avg')\
+        .master('spark://master:7077')\
+        .config('spark.executor.memory', '6g')\
+        .getOrCreate()
+
+    lines = spark.read.text(sys.argv[0]).rdd.map(lambda r: r[0])
+    map_res = lines.map(lambda x: mapper(x)) 
+    map_res.saveAsTextFile(sys.argv[2])
+
